0fc1ab78a25fc0f4ae8aa0cf84758394136cc79b
Jan Winterfeld <jan@markenbegeisterung.de>
2018-06-08 21:26:02 +0200

    static about component and page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,9 +8,11 @@ export default ({ children }) => (
       <Link to="/">
         <img className={styles.logo} src="/assets/christian-brandes-fotograf-logo.png" alt="Christian Brandes - Photograph Logo" />
       </Link>
-      <nav role="navigation" className={styles.navigation}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500"><path d="M0 0h500v92.3H0zM0 407.7h500V500H0zM0 203.9h500v92.3H0z"/></svg>
-      </nav>
+      <Link to="/about">
+        <nav role="navigation" className={styles.navigation}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500"><path d="M0 0h500v92.3H0zM0 407.7h500V500H0zM0 203.9h500v92.3H0z"/></svg>
+        </nav>
+      </Link>
     </header>
     <main id="main" className={styles.content}>
       {children()}
---
+++ src/pages/about.js
@@ -0,0 +1,8 @@
+import React from "react"
+import AboutComponent from "../components/about"
+
+const About = () => (
+  <AboutComponent />
+)
+
+export default About
