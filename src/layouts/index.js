2748a02c6cc4d56376260e261d9fc3ba5f51be8e
Jan Winterfeld <jan@markenbegeisterung.de>
2018-06-10 21:54:34 +0200

    Switch from function to class

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,31 +4,44 @@ import Lightbox from "../components/lightbox"
 import AboutComponent from "../components/about"
 import styles from "./layout.module.scss"
 
-export default ({ children }) => (
-  <div className={styles.page}>
-    <header id="header" className={styles.header}>
-      <Link to="/">
-        <img className={styles.logo} src="/assets/christian-brandes-fotograf-logo.png" alt="Christian Brandes - Photograph Logo" />
-      </Link>
-      <Link to="/about">
-        <nav role="navigation" className={styles.navigation}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500"><path d="M0 0h500v92.3H0zM0 407.7h500V500H0zM0 203.9h500v92.3H0z"/></svg>
-        </nav>
-      </Link>
-    </header>
-    <main id="main" className={styles.content}>
-      {children()}
-    </main>
-    <footer id="footer" className={styles.footer}>
-      <div className={styles.copyright}>
-        &copy; Christian Brandes
+class IndexLayout extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const { children } = this.props
+
+    return (
+      <div className={styles.page}>
+        <header id="header" className={styles.header}>
+          <Link to="/">
+            <img className={styles.logo} src="/assets/christian-brandes-fotograf-logo.png" alt="Christian Brandes - Photograph Logo" />
+          </Link>
+          <Link to="/about">
+            <nav role="navigation" className={styles.navigation}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500"><path d="M0 0h500v92.3H0zM0 407.7h500V500H0zM0 203.9h500v92.3H0z"/></svg>
+            </nav>
+          </Link>
+        </header>
+        <main id="main" className={styles.content}>
+          {children()}
+        </main>
+        <footer id="footer" className={styles.footer}>
+          <div className={styles.copyright}>
+            &copy; Christian Brandes
+          </div>
+          <nav role="navigation" className={styles.secondaryNavigation}>
+            <a href="#">Impressum &amp; Datenschutz</a>
+          </nav>
+        </footer>
+        <Lightbox visible={false}>
+          <AboutComponent />
+        </Lightbox>
       </div>
-      <nav role="navigation" className={styles.secondaryNavigation}>
-        <a href="#">Impressum &amp; Datenschutz</a>
-      </nav>
-    </footer>
-    <Lightbox visible={false}>
-      <AboutComponent />
-    </Lightbox>
-  </div>
-);
+    )
+  }
+}
+
+export default IndexLayout
