c6a34f0b02cfbc2cbd36fb93132363ba17849ea9
Jan Winterfeld <jan@markenbegeisterung.de>
2018-06-21 15:34:34 +0200

    fix siteUrl prod deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,12 @@ const spaceId = process.env.CONTENTFUL_SPACE_ID || ''
 const accessToken = process.env.CONTEXT === 'production' ? process.env.CONTENTFUL_ACCESS_TOKEN : process.env.CONTENTFUL_PREVIEW_TOKEN
 const host = process.env.CONTEXT === 'production' ? 'cdn.contentful.com': 'preview.contentful.com'
 
+const siteUrl = process.env.CONTEXT === 'production' ? process.env.URL : process.env.DEPLOY_PRIME_URL
+
 module.exports = {
+  siteMetadata: {
+    siteUrl
+  },
   plugins: [
     `gatsby-plugin-sass`,
     {
