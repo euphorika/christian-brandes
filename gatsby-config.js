a7645d9d3cb9c73ca02f7161efc4627287b3af6e
Jan Winterfeld <jan@markenbegeisterung.de>
2018-10-25 12:20:47 +0200

    disable offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,6 +56,6 @@ module.exports = {
         icon: "src/images/icon.png",
       },
     },
-    `gatsby-plugin-offline`,
+    //`gatsby-plugin-offline`, // videos not auto playing in safari
   ]
 };
